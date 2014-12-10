49fbfb9124fb6bfc31e51fa02ff95ed238839818
Marko Lalic <marko.lalic@gmail.com>
2014-12-10 13:55:56 +0100

    Add a VideoSource observer for visualizing the PC
    The observer can be attached to a point cloud that should be
    visualized.
    
    The implementation is in terms of PCL's CloudViewer.

---
+++ src/lepp2/visualization/EchoObserver.hpp
@@ -0,0 +1,27 @@
+#ifndef LEPP2_VISUALIZATION_ECHO_OBSERVER_H__
+#define LEPP2_VISUALIZATION_ECHO_OBSERVER_H__
+
+namespace lepp {
+
+/**
+ * Simple VideoObserver implementation that echoes the point cloud to a PCL
+ * CloudViewer for visualization purposes.
+ */
+template<class PointT>
+class EchoObserver : public VideoObserver<PointT> {
+public:
+  EchoObserver() : viewer_("PCL Viewer") {}
+
+  virtual void notifyNewFrame(
+      int idx,
+      const typename pcl::PointCloud<PointT>::ConstPtr& pointCloud) {
+    viewer_.showCloud(pointCloud);
+  }
+
+private:
+  pcl::visualization::CloudViewer viewer_;
+};
+
+}
+
+#endif
