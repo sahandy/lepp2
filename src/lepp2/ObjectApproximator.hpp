e249abc111f0ebd3ad13d6ca7c4cfc47ccf21535
Marko Lalic <marko.lalic@gmail.com>
2014-12-15 22:16:27 +0100

    Add an interface specification for ObjectApproximators
    ObjectApproximators will be classes that provide approximations
    of given point clouds. The approximators do not need to know
    whether the given point cloud is a part of a larger point cloud.
    
    The approximators should be stateless and not have any other
    dependencies to facilitate their testing and future concurrency.
    
    For now, the interface itself contains a dependency on some of
    the legacy code, namely, the ObjectModel. This is in order to
    make a step-by-step refactoring where the initial introduction of
    the approximator keeps the same functionality as previously.

---
+++ src/lepp2/ObjectApproximator.hpp
@@ -0,0 +1,33 @@
+#ifndef LEPP2_OBJECT_APPROXIMATOR_H__
+#define LEPP2_OBJECT_APPROXIMATOR_H__
+
+#include <pcl/common/projection_matrix.h>
+
+/**
+ * Forward declaration of the ObjectModel class that represents an
+ * approximation.
+ */
+class ObjectModel;
+
+namespace lepp {
+
+/**
+ * An abstract base class that classes that wish to generate approximations
+ * for point clouds need to implement.
+ */
+template<class PointT>
+class ObjectApproximator {
+public:
+  /**
+   * Generate the approximations for the given point cloud.
+   * The method assumes that the given point cloud segment is a single physical
+   * object and tries to find the best approximations for this object, using its
+   * own specific approximation method.
+   */
+  virtual std::vector<boost::shared_ptr<ObjectModel> > approximate(
+      const typename pcl::PointCloud<PointT>::ConstPtr& point_cloud) = 0;
+};
+
+}  // namespace lepp
+
+#endif
