b6d7fef8fe53a00435d866d17c5d6ff2e572a0fa
Marko Lalic <marko.lalic@gmail.com>
2014-12-29 19:18:52 +0100

    Handle multiple inputs in obstacle detector exe
    The obstacle detector executable can now handle different input
    formats, chosen by a command line argument, automatically.
    
    It also does not automatically exit after a certain amount of time.
    For the demonstration purposes, this seems a much better choice,
    than having to restart the executable every-so-often.

--- src/lepp2/detector/main.cc
+++ src/lepp2/detector/main.cc
@@ -5,6 +5,7 @@
 #include <iostream>
 
 #include <pcl/io/openni2_grabber.h>
+#include <pcl/io/pcd_grabber.h>
 
 #include "lepp2/BaseObstacleDetector.hpp"
 #include "lepp2/GrabberVideoSource.hpp"
@@ -20,26 +21,56 @@ using namespace lepp;
  * Prints out the expected CLI usage of the program.
  */
 void PrintUsage() {
-  std::cout << "The program expects a single argument: a path to an .oni file"
-      << " which will serve as the input to the obstacle detector" << std::endl;
+  std::cout << "usage: detector [--pcd file | --oni file | --stream]"
+      << std::endl;
+  std::cout << "--pcd    : " << "read the input from a .pcd file" << std::endl;
+  std::cout << "--oni    : " << "read the input from an .oni file" << std::endl;
+  std::cout << "--stream : " << "read the input from a live stream based on a"
+      << " sensor attached to the computer" << std::endl;
 }
 
+/**
+ * Parses the command line arguments received by the program and chooses
+ * the appropriate video source based on those.
+ */
+boost::shared_ptr<SimpleVideoSource> GetVideoSource(int argc, char* argv[]) {
+  if (argc < 2) {
+    return boost::shared_ptr<SimpleVideoSource>();
+  }
+
+  std::string const option = argv[1];
+  if (option == "--stream" && argc == 2) {
+    return boost::shared_ptr<SimpleVideoSource>(
+        new LiveStreamSource<SimplePoint>());
+  } else if (option == "--pcd" && argc == 3) {
+    std::string const file_path = argv[2];
+    boost::shared_ptr<pcl::Grabber> interface(new pcl::PCDGrabber<SimplePoint>(
+      file_path,
+      20.,
+      true));
+    return boost::shared_ptr<SimpleVideoSource>(
+        new GeneralGrabberVideoSource<SimplePoint>(interface));
+  } else if (option == "--oni" && argc == 3) {
+    std::string const file_path = argv[2];
+    boost::shared_ptr<pcl::Grabber> interface(new pcl::io::OpenNI2Grabber(
+      file_path,
+      pcl::io::OpenNI2Grabber::OpenNI_Default_Mode,
+      pcl::io::OpenNI2Grabber::OpenNI_Default_Mode));
+    return boost::shared_ptr<SimpleVideoSource>(
+        new GeneralGrabberVideoSource<SimplePoint>(interface));
+  }
+
+  // Unknown option: return a "null" pointer.
+  return boost::shared_ptr<SimpleVideoSource>();
+}
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
+  // Obtain a video source based on the command line arguments received
+  boost::shared_ptr<SimpleVideoSource> source(GetVideoSource(argc, argv));
+  if (!source) {
     PrintUsage();
     return 1;
   }
-
-  std::string const file_path = argv[1];
-
-  // Prepare the source: read from the given file
-  boost::shared_ptr<pcl::Grabber> interface(new pcl::io::OpenNI2Grabber(
-    file_path,
-    pcl::io::OpenNI2Grabber::OpenNI_Default_Mode,
-    pcl::io::OpenNI2Grabber::OpenNI_Default_Mode));
-  boost::shared_ptr<SimpleVideoSource> source(
-      new GeneralGrabberVideoSource<SimplePoint>(interface));
 
   // Prepare the detector
   boost::shared_ptr<BaseObstacleDetector<SimplePoint> > detector(
@@ -61,9 +92,10 @@ int main(int argc, char* argv[]) {
   // Starts capturing new frames and forwarding them to attached observers.
   source->open();
 
-  std::cout << "Waiting for 8 seconds..." << std::endl;
-  boost::this_thread::sleep(boost::posix_time::milliseconds(8000));
-  std::cout << "Example finished." << std::endl;
+  std::cout << "Waiting forever..." << std::endl;
+  std::cout << "(^C to exit)" << std::endl;
+  while (true)
+    boost::this_thread::sleep(boost::posix_time::milliseconds(8000));
 
   return 0;
 }
