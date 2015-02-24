bf391fea2a37ac5874679c9b22b624c7d4cf135b
Marko Lalic <marko.lalic@gmail.com>
2015-02-24 15:29:17 +0100

    Add a PT1 filter

--- src/lepp2/FilteredVideoSource.hpp
+++ src/lepp2/FilteredVideoSource.hpp
@@ -271,4 +271,45 @@ private:
   boost::unordered_map<MapPoint, boost::circular_buffer<bool> > all_points_;
 };
 
+/**
+ * An implementation of a `FilteredVideoSource` that applies a PT1 filter on the
+ * stream of clouds.
+ */
+template<class PointT>
+class Pt1FilteredVideoSource : public FilteredVideoSource<PointT> {
+public:
+  Pt1FilteredVideoSource(boost::shared_ptr<VideoSource<PointT> > source)
+      : FilteredVideoSource<PointT>(source) {}
+protected:
+  void newFrame() { this_frame_.clear(); }
+
+  void newPoint(PointT& p, PointCloudType& filtered) {
+    MapPoint map_point = MapPoint(p.x * 100, p.y * 100, p.z * 100);
+    float& ref = all_points_[map_point];
+    // TODO Factor out the parameter value to a member constant.
+    ref = 0.9*ref + 0.1*10;
+    this_frame_.insert(map_point);
+  }
+
+  void getFiltered(PointCloudType& filtered) {
+    boost::unordered_map<MapPoint, float>::iterator it =
+        all_points_.begin();
+    while (it != all_points_.end()) {
+      if (this_frame_.find(it->first) == this_frame_.end()) {
+        it->second = 0.9*it->second + 0.1*0.;
+      }
+      if (it->second >= 4.) {
+        PointT pt;
+        pt.x = it->first.x / 100.;
+        pt.y = it->first.y / 100.;
+        pt.z = it->first.z / 100.;
+        filtered.push_back(pt);
+      }
+      ++it;
+    }
+  }
+private:
+  boost::unordered_set<MapPoint> this_frame_;
+  boost::unordered_map<MapPoint, float> all_points_;
+};
 #endif
