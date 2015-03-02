60e7d0608ebbe5385316bce961c9ecf3c4722dbc
Marko Lalic <marko.lalic@gmail.com>
2015-03-02 17:46:45 +0100

    Implement an obstacle processor that sends notifications to the robot

--- src/lola/LolaAggregator.hpp
+++ src/lola/LolaAggregator.hpp
@@ -2,6 +2,8 @@
 #define LEPP2_LOLA_LOLA_AGGREGATOR_H__
 
 #include "lepp2/ObstacleAggregator.hpp"
+#include "lepp2/DiffAggregator.hpp"
+#include "lola/RobotService.h"
 
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
@@ -20,6 +22,12 @@ namespace {
 
 using namespace lepp;
 
+namespace {
+/**
+ * A `ModelVisitor` implementation used for the implementation of the
+ * `LolaAggregator`. Allows us to obtain all information that is required to
+ * assemble a message for the visualizer.
+ */
 class ParametersVisitor : public lepp::ModelVisitor {
 public:
   void visitSphere(SphereModel& sphere) {
@@ -52,6 +60,7 @@ public:
 private:
   std::vector<double> params_;
 };
+}  // namespace <anonymous>
 
 /**
  * A LOLA-specific implementation of an `ObstacleAggregator`.
@@ -158,5 +167,221 @@ std::vector<char> LolaAggregator::buildPayload(
 
   return payload;
 }
+
+namespace {
+/**
+ * A `ModelVisitor` implementation used for the implementation of the
+ * `RobotAggregator`. Allows us to obtain all information that is required to
+ * assemble a message for the robot.
+ *
+ * Similar to the `ParametersVisitor`, but for the sake of convenience of
+ * the two aggregators' implementations, they are not reconciled.
+ */
+class CoefsVisitor : public lepp::ModelVisitor {
+public:
+  void visitSphere(SphereModel& sphere) {
+    coefs_.push_back(sphere.center().x);
+    coefs_.push_back(sphere.center().y);
+    coefs_.push_back(sphere.center().z);
+    for (size_t i = 0; i < 6; ++i) coefs_.push_back(0);
+
+    type_id_ = 0;
+    radius_ = sphere.radius();
+  }
+
+  void visitCapsule(CapsuleModel& capsule) {
+    coefs_.push_back(capsule.first().x);
+    coefs_.push_back(capsule.first().y);
+    coefs_.push_back(capsule.first().z);
+    coefs_.push_back(capsule.second().x);
+    coefs_.push_back(capsule.second().y);
+    coefs_.push_back(capsule.second().z);
+    for (size_t i = 0; i < 3; ++i) coefs_.push_back(0);
+
+    type_id_ = 1;
+    radius_ = capsule.radius();
+  }
+
+  std::vector<double> const& coefs() const { return coefs_; }
+  double radius() const { return radius_; }
+  int type_id() const { return type_id_; }
+private:
+  std::vector<double> coefs_;
+  int type_id_;
+  double radius_;
+};
+}  // namespace <anonymous>
+
+/**
+ * An `ObstacleAggregator` implementation that sends notifications to the robot
+ * after every certain amount of frames, informing it of changes in the known
+ * obstacles since the previous message.
+ *
+ * The implementation relies on a `DiffAggregator` to find this diff and uses
+ * this to send appropriate messages to the robot. The most notable adjustment
+ * that needs to be made is "flattening" a composite model (one made of several
+ * primitive models) into its most primitive components and sending msesages for
+ * each of those separately to the robot.
+ */
+class RobotAggregator : public lepp::ObstacleAggregator {
+public:
+  RobotAggregator(RobotService& service, int freq)
+      : service_(service), diff_(freq), next_id_(0) {
+    // Set up the callbacks that handle the particular cases.
+    diff_.set_new_callback(boost::bind(&RobotAggregator::new_cb_, this, _1));
+    diff_.set_modified_callback(boost::bind(&RobotAggregator::mod_cb_, this, _1));
+    diff_.set_deleted_callback(boost::bind(&RobotAggregator::del_cb_, this, _1));
+  }
+  /**
+   * `ObstacleAggregator` interface implementation.
+   */
+  void updateObstacles(std::vector<ObjectModelPtr> const& obstacles) {
+    // Just pass it on to find the diff!
+    diff_.updateObstacles(obstacles);
+  }
+private:
+  /**
+   * The function is passed as a callback to the underlying `DiffAggregator` for
+   * when new models are discovered.
+   */
+  void new_cb_(ObjectModel& model) {
+    // Assign an ID to each part of the new model.
+    std::vector<ObjectModel*> primitives(getPrimitives(model));
+    size_t const sz = primitives.size();
+    std::vector<int>& ids = robot_ids_[model.id()];
+    for (size_t i = 0; i < sz; ++i) {
+      // Assign it a new ID
+      int const id = nextId();
+      ids.push_back(id);
+      // ...and send a message to the robot.
+      sendNew(*primitives[i], id);
+    }
+  }
+
+  /**
+   * The function is passed as a callback to the underlying `DiffAggregator` for
+   * when models are discovered to be deleted.
+   */
+  void del_cb_(int obj_id) {
+    // Delete all parts of the deleted object.
+    {
+      std::vector<int>& ids = robot_ids_[obj_id];
+      for (size_t i = 0; i < ids.size(); ++i) {
+        sendDelete(ids[i]);
+      }
+    }
+    // Remove it from the map too.
+    robot_ids_.erase(obj_id);
+  }
+
+  /**
+   * The function is passed as a callback to the underlying `DiffAggregator` for
+   * when a model has been modified.
+   */
+  void mod_cb_(ObjectModel& model) {
+    std::vector<ObjectModel*> primitives(getPrimitives(model));
+    size_t const new_size = primitives.size();
+    std::vector<int>& ids = robot_ids_[model.id()];
+    size_t const old_size = ids.size();
+
+    if (new_size < old_size) {
+      // Some parts need to be deleted.
+      for (size_t i = 0; i < old_size - new_size; ++i) {
+        sendDelete(ids[old_size - i - 1]);
+        ids.pop_back();
+      }
+    }
+
+    // Now we modify what we have left from before
+    size_t const sz = ids.size();
+    for (size_t i = 0; i < sz; ++i) {
+      sendModify(*primitives[i], ids[i]);
+    }
+
+    // And finally add new ones, if necessary
+    if (new_size > old_size) {
+      for (size_t i = 0; i < new_size - old_size; ++i) {
+        int const id = nextId();
+        ids.push_back(id);
+        sendNew(*primitives[old_size + i], id);
+      }
+    }
+  }
+
+  /**
+   * Obtains a list of pointers to the primitives that the given model is
+   * composed of.
+   * This list is safe to use only while the model reference is in scope.
+   */
+  std::vector<ObjectModel*> getPrimitives(ObjectModel& model) const {
+    FlattenVisitor flattener;
+    model.accept(flattener);
+    return flattener.objs();
+  }
+
+  /**
+   * Sends a message to the robot informing it of a new model.
+   */
+  void sendNew(ObjectModel& new_model, int id) {
+    CoefsVisitor coefs;
+    new_model.accept(coefs);
+    VisionMessage msg = VisionMessage::SetMessage(
+        coefs.type_id(), id, coefs.radius(), coefs.coefs());
+    std::cerr << "RobotAggregator: Creating new primitive ["
+              << "type = " << coefs.type_id()
+              << "; id = " << id << std::endl;
+    service_.sendMessage(msg);
+  }
+
+  /**
+   * Sends a message to the robot informing it of a deleted model.
+   */
+  void sendDelete(int id) {
+    std::cerr << "RobotAggregator: Deleting a primitive id = "
+              << id << std::endl;
+    VisionMessage del = VisionMessage::DeleteMessage(id);
+    service_.sendMessage(del);
+  }
+
+  /**
+   * Sends a message to the robot informing it of a modified model.
+   */
+  void sendModify(ObjectModel& model, int id) {
+    CoefsVisitor coefs;
+    model.accept(coefs);
+    VisionMessage msg = VisionMessage::ModifyMessage(
+        coefs.type_id(), id, coefs.radius(), coefs.coefs());
+    std::cerr << "RobotAggregator: Modifying existing primitive ["
+              << "type = " << coefs.type_id()
+              << "; id = " << id << std::endl;
+    service_.sendMessage(msg);
+  }
+
+  /**
+   * Obtains the next ID that should be used for a primitive that the robot is
+   * notified of.
+   */
+  int nextId() { return next_id_++; }
+
+  /**
+   * A handle to the service that is used to send notifications to the robot.
+   */
+  RobotService& service_;
+  /**
+   * An instance of a `DiffAggregator` that this one delegates to in order to
+   * detect the differences between the checkpoint frames.
+   */
+  DiffAggregator diff_;
+
+  /**
+   * Maps the approximation ID to a list of IDs that the robot will know for
+   * each primitive SSV object that is found in the model composition.
+   */
+  std::map<int, std::vector<int> > robot_ids_;
+  /**
+   * The ID that can be assigned to the next new model (or rather model part).
+   */
+  int next_id_;
+};
 
 #endif
