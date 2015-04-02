499bfa108743e1f92803b3c3aa7ffadc9b804fda
Marko Lalic <marko.lalic@gmail.com>
2015-04-02 14:19:08 +0200

    Add a `Robot` class: a facade in front of robot services
    The `Robot` class will expose an API that unifies the different
    robot-related services and provides additional robot information
    by combining the different information provided by different services.

---
+++ src/lola/Robot.h
@@ -0,0 +1,19 @@
+#ifndef LOLA_ROBOT_H__
+#define LOLA_ROBOT_H__
+
+#include "lola/PoseService.h"
+#include "lepp2/models/ObjectModel.h"
+
+/**
+ * A facade in front of different services and methods that the robot
+ * supports.
+ *
+ * It wraps a number of services and provides a unified interface over
+ * their methods. Additionally, it provides some robot-specific functionality
+ * based on the information provided by the services.
+ */
+class Robot {
+public:
+};
+
+#endif
