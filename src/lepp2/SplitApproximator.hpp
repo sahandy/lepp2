18f6b7882eb940523155d0a424006f2bf4d338b1
Marko Lalic <marko.lalic@gmail.com>
2015-04-18 21:41:54 +0200

    Add a SplitCondition base class

--- src/lepp2/SplitApproximator.hpp
+++ src/lepp2/SplitApproximator.hpp
@@ -137,6 +137,28 @@ SplitStrategy<PointT>::doSplit(
 }
 
 /**
+ * An ABC for classes that provide the functionality of checking whether a
+ * point cloud should be split or not.
+ */
+template<class PointT>
+class SplitCondition {
+public:
+  /**
+   * A pure virtual method that decides whether the given point cloud should be
+   * split or not.
+   *
+   * :param split_depth: The current split depth, i.e. the number of times the
+   *     original cloud has already been split
+   * :param point_cloud: The current point cloud that should be split by the
+   *    `SplitStrategy` implementation.
+   * :returns: A boolean indicating whether the cloud should be split or not.
+   */
+  virtual bool shouldSplit(
+      int split_depth,
+      const typename pcl::PointCloud<PointT>::ConstPtr& point_cloud) = 0;
+};
+
+/**
  * A `SplitStrategy` implementation that initiates the split iff the depth is
  * less than the given limit.
  */
