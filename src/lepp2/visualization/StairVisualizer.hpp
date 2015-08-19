0223099f3f5dc2c79be67d2cd98d544110561245
sahandy <y.sahand@gmail.com>
2015-08-19 13:04:15 +0200

    Stairviz Mimics Same Behavior as ObstacleViz
    For the sake of testing, StairVisualizer is built based on
    same behaviors as the ObstacleVisualizer. It contains a
    cloud viewer and a PCL Visualizer which overlays stairs
    on top of it.
    The visualizer is still problematic. It seems on some systems
    it faces "Segmentation Fault" error.
    TODO: Check if downgrade to OpenCV 2.4.9 solves this problem.

--- src/lepp2/visualization/StairVisualizer.hpp
+++ src/lepp2/visualization/StairVisualizer.hpp
@@ -11,81 +11,123 @@
 namespace lepp{
 
 template<class PointT>
+class StairDrawer {
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  StairDrawer(pcl::visualization::PCLVisualizer& pcl_viz)
+    : pcl_viz_(pcl_viz), total_(0) {}
+  void drawStair(typename pcl::PointCloud<PointT>::ConstPtr cloud_stair);
+private:
+  pcl::visualization::PCLVisualizer& pcl_viz_;
+  int total_;
+};
+
+template<class PointT>
+void StairDrawer<PointT>::drawStair(
+    typename pcl::PointCloud<PointT>::ConstPtr cloud_stair)  {
+      ++total_;
+
+      typename pcl::PointCloud<PointT>::Ptr single_cloud (new pcl::PointCloud<PointT> ());
+      pcl::visualization::PointCloudColorHandlerCustom<PointT> red (single_cloud, 255, 0, 0);
+      pcl::visualization::PointCloudColorHandlerCustom<PointT> green (single_cloud, 0, 255, 0);
+      pcl::visualization::PointCloudColorHandlerCustom<PointT> blue (single_cloud, 0, 0, 255);
+      pcl::visualization::PointCloudColorHandlerCustom<PointT> pink (single_cloud, 200, 18, 170);
+
+      std::ostringstream ss;
+      ss << "STAIR_" << total_;
+      std::string const cloud_name = ss.str();
+
+      single_cloud->clear ();
+      *single_cloud = *cloud_stair;
+      if (total_ == 1)
+      {
+        std::cout << "show cloud #1" << std::endl;
+        pcl_viz_.addPointCloud (single_cloud, red, cloud_name);
+      }
+      else if (total_ == 2)
+      {
+        std::cout << "show cloud #2" << std::endl;
+        pcl_viz_.addPointCloud (single_cloud, green, cloud_name);
+      }
+      else if (total_ == 3)
+      {
+        std::cout << "show cloud #3" << std::endl;
+        pcl_viz_.addPointCloud (single_cloud, blue, cloud_name);
+      }
+      else
+      {
+        std::cout << "show cloud #" << total_ << std::endl;
+        pcl_viz_.addPointCloud (single_cloud, pink, cloud_name);
+      }
+
+    }
+
+template<class PointT>
 class StairVisualizer
   : public VideoObserver<PointT>,
     public StairAggregator<PointT> {
 public:
-  StairVisualizer()
-    : pcl_visualizer_(new pcl::visualization::PCLVisualizer ("STAIR_VISUALIZER")),
-      is_first_cloud(true),
-      is_first_addition(true) {
-
-    pcl_visualizer_->setBackgroundColor (0, 0, 0);
-//    pcl_visualizer_->addPointCloud < pcl::PointXYZ > (cloud, "sample cloud");
-
-    pcl_visualizer_->addCoordinateSystem (1.0);
-    pcl_visualizer_->initCameraParameters ();
-
-    }
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  StairVisualizer() : viewer_("StairVisualizer")
+                       {} //pclViz (new pcl::visualization::PCLVisualizer ("PCL_VIZ"))
 
   /**
-   * VideoObserver interface implementation: processes the current point cloud.
+   * VideoObserver interface implementation: show the current point cloud.
    */
   virtual void notifyNewFrame(
       int idx,
       const typename pcl::PointCloud<PointT>::ConstPtr& pointCloud) {
-
-	  if (is_first_cloud) {
-		  pcl_visualizer_->addPointCloud(pointCloud, "TOTALPC");
-		  pcl_visualizer_->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "TOTALPC");
-		  is_first_cloud = false;
-    }
-    else
-      pcl_visualizer_->updatePointCloud(pointCloud, "TOTALPC");
-    pcl_visualizer_->spinOnce(1);
-  }
+        viewer_.showCloud(pointCloud);
+      }
 
   /**
    * StairAggregator interface implementation: processes detected obstacles.
    */
-//  virtual void updateStairs(std::vector<typename pcl::PointCloud<PointT>::ConstPtr> stairs);
   virtual void updateStairs(std::vector<typename pcl::PointCloud<PointT>::ConstPtr> cloud_stairs);
+
+  void drawStairs(std::vector<typename pcl::PointCloud<PointT>::ConstPtr> stairs,
+                  pcl::visualization::PCLVisualizer& viewer);
 
 private:
   /**
    * Used for the visualization of the scene.
    */
-  boost::shared_ptr<pcl::visualization::PCLVisualizer> pcl_visualizer_;
-  bool is_first_cloud;
-  bool is_first_addition;
+  pcl::visualization::CloudViewer viewer_;
+  // boost::shared_ptr<pcl::visualization::PCLVisualizer> pclViz;
 };
 
 template<class PointT>
-void StairVisualizer<PointT>::updateStairs(std::vector<typename pcl::PointCloud<PointT>::ConstPtr> cloud_stairs) {
+void StairVisualizer<PointT>::drawStairs(
+    std::vector<typename pcl::PointCloud<PointT>::ConstPtr> stairs,
+    pcl::visualization::PCLVisualizer& pclViz) {
+  // Remove all old stars...
+  std::cout << "call removeAllPointClouds" << std::endl;
+  pclViz.removeAllPointClouds();
+  std::cout << "all pointClouds removed" << std::endl;
 
+  // ...and draw all the new ones.
+  StairDrawer<PointT> drawer(pclViz);
+  size_t const sz = stairs.size();
+  for (size_t i = 0; i < sz; ++i) {
+    drawer.drawStair(stairs[i]);
+  }
 
-    typename pcl::PointCloud<PointT>::Ptr single_cloud(new pcl::PointCloud<PointT>());
-    size_t const sz = cloud_stairs.size();
-    for(size_t i=0; i<sz; ++i) {
-      // first plane is always the floor (a.k.a biggest plane found by StairSegmenter)
-      if(i==0)
-        continue;
+  std::cout << "exiting stair drawer..." << std::endl;
+  std::cout << "=======================" << std::endl;
+}
 
-      *single_cloud = *cloud_stairs[i];
-      // TODO Assign Different color to each cloud
-      pcl::visualization::PointCloudColorHandlerCustom<PointT> single_color (single_cloud, 0, 255, 0);
-      if(is_first_addition) {
-        pcl_visualizer_->addPointCloud(single_cloud, single_color, "STAIRS");
-        pcl_visualizer_->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "STAIRS");
+template<class PointT>
+void StairVisualizer<PointT>::updateStairs(
+    std::vector<typename pcl::PointCloud<PointT>::ConstPtr> stairs) {
+    std::cout << "entered updateStairs" << std::endl;
+  pcl::visualization::CloudViewer::VizCallable stair_visualization =
+        boost::bind(&StairVisualizer::drawStairs,
+                    this, stairs, _1);
 
-        is_first_addition = false;
-      } else
-        pcl_visualizer_->updatePointCloud(single_cloud, single_color, "STAIRS");
-    }
-
-    pcl_visualizer_->spinOnce(1);
+  std::cout << "call runOnVisualizationThreadOnce" << std::endl;
+  viewer_.runOnVisualizationThreadOnce(stair_visualization);
+  std::cout << "exiting updateStairs..." << std::endl;
 }
 
 } // namespace lepp
-
 #endif
